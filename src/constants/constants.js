789af1e0b1f95f67d42a456cc6d8118edc405b23
MUHESI <mvmmuhesi@gmail.com>
2023-05-29 11:29:04 +0200

    move all images of projects to cloudinary

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -3,7 +3,8 @@ export const projects = [
     title: "Acho-System, 2023-now",
     description:
       "AS is an IT platform that combines the functionalities of e-businesses commerce and fintech.",
-    image: "/images/acho/acho.png",
+    image:
+      "https://res.cloudinary.com/chanel-muhesi/image/upload/v1685352056/porfolio/acho_wlzkrj.png",
     tags: ["Redis", "PostgreSQL ", "Microservices", "Nestjs", "Typescript"],
     source: "http://localhost:3000/#",
     visit: "http://localhost:3000/#",
@@ -12,7 +13,8 @@ export const projects = [
   {
     title: "E-EVOGYM clone app 2023-2023",
     description: "A website which expose the Evogym services.",
-    image: "/images/sport.png",
+    image:
+      "https://res.cloudinary.com/chanel-muhesi/image/upload/v1685352439/porfolio/sport_tq5cqe.png",
     tags: ["Nextjs", "NotioReact", "Tailwind", "MUI"],
     source: "https://main-gym.vercel.app/",
     visit: "https://main-gym.vercel.app/",
@@ -24,7 +26,8 @@ export const projects = [
     description:
       "I built  Bellevue platform, It manages internal services in this I/O (more than 60 000 customers to manage in this platform)",
 
-    image: "/images/bellevue/bellevueDashBoard.png",
+    image:
+      "https://res.cloudinary.com/chanel-muhesi/image/upload/v1685352260/porfolio/bellevueDashBoard_zrvouz.png",
     tags: ["Mongo", "Express", "TS", "React", "SCSS", "MUI"],
     source: "http://localhost:3000/#",
     visit: "http://localhost:3000/#",
@@ -34,7 +37,8 @@ export const projects = [
   {
     title: "Join-Africa 2021-2022",
     description: "web app wich connects service seekers with service providers",
-    image: "/images/join-afrik/joinAfrik.png",
+    image:
+      "https://res.cloudinary.com/chanel-muhesi/image/upload/v1685352180/porfolio/joinAfrik_v0ohmg.png",
     tags: ["Express", "Mongo", "TS", "React", "Bootstrap"],
     source: "https://join-front-c9xt.vercel.app/",
     visit: "https://join-front-c9xt.vercel.app/",
@@ -44,7 +48,8 @@ export const projects = [
     title: "FaceRecogn-app RDC 2021-2022",
     description:
       "I built a platform for identifying people through facial recognition. Basically this system is designed to be used by DGM service agents  to control inputs and outputs, and also to match people's photos with traveler identities in the reference database.",
-    image: "/images/rf/rf.png",
+    image:
+      "https://res.cloudinary.com/chanel-muhesi/image/upload/v1685352348/porfolio/rf_ookjxr.png",
     tags: ["FaceApi", "MySql", "TS", "React", "MUI"],
     source: "http://localhost:3000/#",
     visit: "http://localhost:3000/#",
@@ -53,7 +58,8 @@ export const projects = [
   {
     title: "Manager rating sheet 2019-2021",
     description: "A platform for managing teacher rating sheets at university.",
-    image: "/images/gestionFiche.png",
+    image:
+      "https://res.cloudinary.com/chanel-muhesi/image/upload/v1685352507/porfolio/gestionFiche_xwdmbx.png",
     tags: ["MongoDb", "Mongo", "React", "Tailwind", "MUI"],
     source: "http://localhost:3000/#",
     visit: "http://localhost:3000/#",
@@ -63,7 +69,8 @@ export const projects = [
     title: "E-Commerce app 2020-2021",
     description:
       "E-commerce (front) is a front project that integrates some e-commerce features(Create an account, View items, See the details of an item,Create a basket, Validate the commands of client)",
-    image: "/images/e-commerce.png",
+    image:
+      "https://res.cloudinary.com/chanel-muhesi/image/upload/v1685351787/porfolio/e-commerce_fouvgq.png",
     tags: ["MongoDb", "Mongo", "React", "Tailwind", "MUI"],
     source: "https://muhesi.github.io/poo-e-commerce-front/",
     visit: "https://muhesi.github.io/poo-e-commerce-front/",
